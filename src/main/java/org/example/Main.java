3b55b5e4b17c75fd79ed192088cc29a1458dd7a2
Denis <67637009+Denis-prig@users.noreply.github.com>
2022-09-19 21:57:06 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println("Hello world!");
+        int i =12;
+        
+        System.out.println("test merge!");
         System.out.println("Hello !");
         
         
